68e6ce29ff6829c4ca94030467e6a6f8146d1f1f
erick-costa <erickcosta2509@gmail.com>
2024-04-26 14:39:26 -0300

    inicialização do projeto

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,16 @@
+import "./Home.css"
+
+function Home() {
+  return (
+    <>
+      <h1 className="titulo">Home</h1>
+      <img
+        className="img"
+        src="https://imagens-revista.vivadecora.com.br/uploads/2019/07/Home-para-sala-pequena.jpg"
+        alt=""
+      />
+    </>
+  )
+}
+
+export default Home
