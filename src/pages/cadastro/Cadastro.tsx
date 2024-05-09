81809a1373a3bce3a4d6586345aebc3b3d487940
erick-costa <erickcosta2509@gmail.com>
2024-05-09 12:03:22 -0300

    perfil e alertas com toastAlert

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -1,8 +1,9 @@
-import React, { ChangeEvent, useEffect, useState } from "react"
+import { ChangeEvent, useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import Usuario from "../../models/Usuario"
 import { cadastrarUsuario } from "../../services/Service"
 import "./Cadastro.css"
+import { toastAlerta } from "../../utils/toastAlerta"
 
 function Cadastro() {
   let navigate = useNavigate()
@@ -56,12 +57,15 @@ function Cadastro() {
           usuario,
           setUsuarioResposta
         )
-        alert("Usuário cadastrado com sucesso")
+        toastAlerta("Usuário cadastrado com sucesso", "sucesso")
       } catch (error) {
-        alert("Erro ao cadastrar o Usuário")
+        toastAlerta("Usuário cadastrado com sucesso", "sucesso")
       }
     } else {
-      alert("Dados inconsistentes. Verifique as informações de cadastro.")
+      toastAlerta(
+        "Dados inconsistentes. Verifique as informações de cadastro.",
+        "erro"
+      )
       setUsuario({ ...usuario, senha: "" }) // Reinicia o campo de Senha
       setConfirmaSenha("") // Reinicia o campo de Confirmar Senha
     }
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -50,7 +50,7 @@ function Login() {
               type="text"
               id="usuario"
               name="usuario"
-              placeholder="Usuário"
+              placeholder="Usuario"
               className="border-2 border-slate-700 rounded p-2"
               value={usuarioLogin.usuario}
               onChange={(e: ChangeEvent<HTMLInputElement>) =>
